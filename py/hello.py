57117e54be1d49f89ab4c238bd2cc974eada2425
chainke <chainke@techfak.uni-bielefeld.de>
2017-06-27 11:26:26 +0200

    can now access clients loign date

--- py/hello.py
+++ py/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, url_for, render_template, escape, redirect, session, jsonify
+from datetime import datetime
 app = Flask(__name__)
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
@@ -12,11 +13,14 @@ def hello_world():
 @app.route('/')
 def index():
 	#return render_template('index.html');
-	if 'username' in session and 'ip' in session:
+	if 'username' in session and 'ip' in session and 'date' in session:
 		username = session['username']
 		ip = session['ip']
+		date = session['date']
+		date_string = date.strftime('%d.%m.%Y %H:%M:%S')
 		return 'Logged in as ' + username + '<br>' + \
 		'IP: ' + ip + '<br>' + \
+		'Date: ' + date_string + '<br>' + \
 		"<b><a href = '/logout'>click here to log out</a></b>"
 	return "You are not logged in <br><a href = '/login'></b>" + \
 	"click here to log in</b></a>"
@@ -28,6 +32,9 @@ def login():
 		session['username'] = request.form['username']
 		#session['ip'] = jsonify({'ip': request.remote_addr}), 200
 		session['ip'] = request.environ['REMOTE_ADDR']
+		session['date'] = datetime.now()
+		# info contains CPU, GPU, RAM, ...		
+		#session['info'] =
 		return redirect(url_for('index'))
 	return '''
 	
