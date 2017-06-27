e0998f7e84b91ffde32f7969063635df6d4ca68c
chainke <chainke@techfak.uni-bielefeld.de>
2017-06-27 11:10:22 +0200

    can now access clients ip address

--- py/hello.py
+++ py/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, render_template, escape, redirect, session
+from flask import Flask, request, url_for, render_template, escape, redirect, session, jsonify
 app = Flask(__name__)
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
@@ -12,31 +12,36 @@ def hello_world():
 @app.route('/')
 def index():
 	#return render_template('index.html');
-   if 'username' in session:
-      username = session['username']
-      return 'Logged in as ' + username + '<br>' + \
-      "<b><a href = '/logout'>click here to log out</a></b>"
-   return "You are not logged in <br><a href = '/login'></b>" + \
-      "click here to log in</b></a>"
+	if 'username' in session and 'ip' in session:
+		username = session['username']
+		ip = session['ip']
+		return 'Logged in as ' + username + '<br>' + \
+		'IP: ' + ip + '<br>' + \
+		"<b><a href = '/logout'>click here to log out</a></b>"
+	return "You are not logged in <br><a href = '/login'></b>" + \
+	"click here to log in</b></a>"
 
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-   if request.method == 'POST':
-      session['username'] = request.form['username']
-      return redirect(url_for('index'))
-   return '''
+	if request.method == 'POST':
+		session['username'] = request.form['username']
+		#session['ip'] = jsonify({'ip': request.remote_addr}), 200
+		session['ip'] = request.environ['REMOTE_ADDR']
+		return redirect(url_for('index'))
+	return '''
 	
-   <form action = "" method = "post">
+	<form action = "" method = "post">
       <p><input type = text name = 'username' /></p>
       <p><input type = submit value = 'Login' /></p>
-   </form> 
+	</form> 
 	'''
 
 @app.route('/logout')
 def logout():
    # remove the username from the session if it is there
    session.pop('username', None)
+   session.pop('ip', None)
    return redirect(url_for('index'))
 
 
