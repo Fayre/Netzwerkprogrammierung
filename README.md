03fe9af9aea3018a6947f508732416d3d13bac26
chainke <chainke@techfak.uni-bielefeld.de>
2017-06-29 14:07:41 +0200

    added client memory and processor

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Netzwerkprogrammierung
 
-To start the server: \n
-navigate to server.py \n
-$ export FLASK_APP=server.py \n
-$ python -m flask run \n
+To start the server: 
+* navigate to server.py 
+* $ export FLASK_APP=server.py 
+* $ python -m flask run 
 
 
 To start a client:
-navigate to localhost in Webbrowser
+* navigate to localhost in Webbrowser
--- py/server.py
+++ py/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, url_for, render_template, escape, redirect, session, jsonify
 from datetime import datetime
+import platform
+import psutil
+
 app = Flask(__name__)
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
@@ -15,6 +18,11 @@ def hello_world():
 def index():
 	#return render_template('index.html');
 	if 'username' in session and 'ip' in session and 'date' in session and 'info' in session:
+		# get system info
+		processor = platform.processor()
+		memory = psutil.virtual_memory()
+
+		# get session data
 		username = session['username']
 		ip = session['ip']
 		date = session['date']
@@ -28,6 +36,8 @@ def index():
 		'CPU: ' + cpu + '<br>' + \
 		'GPU: ' + gpu + '<br>' + \
 		'RAM: ' + ram + '<br>' + \
+		'Processor: ' + processor + '<br>' + \
+		'Total Memory: ' + str(memory[1]) + '<br>' + \
 		"<b><a href = '/logout'>click here to log out</a></b>"
 	return "You are not logged in <br><a href = '/login'></b>" + \
 	"click here to log in</b></a>"
@@ -37,7 +47,6 @@ def index():
 def login():
 	if request.method == 'POST':
 		session['username'] = request.form['username']
-		#session['ip'] = jsonify({'ip': request.remote_addr}), 200
 		session['ip'] = request.environ['REMOTE_ADDR']
 		session['date'] = datetime.now()
 		# info contains CPU, GPU, RAM, ...
@@ -57,5 +66,25 @@ def logout():
    session.pop('ip', None)
    return redirect(url_for('index'))
 
+
+
+
+
+#@app.route('/hardware_test')
+#def hardware_test():
+#	try:
+#		cpu_genric_info = cpu_generic_details()
+#	except Exception as ex:
+#		print ex
+#	finally:
+#		return render_template("hardware_test_index.html", title="hardware_test_index", cpu_generic_info = cpu_generic_info)
+
+#def cpu_generic_details():
+#	try:
+#		items = [s.split('\t: ') for s in subprocess.check_output(["cat /proc/cpuinfo | grep 'model name\|Hardware\|Serial' | uniq "], shell=True).splitlines()]
+#	except Exception as ex:   
+#		print ex
+#	finally:
+#		return items
 
 
